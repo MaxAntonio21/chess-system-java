285ce47c8f4d3ad68c8ec4108921741059edeeca
Max Antonio <max15antonio@hotmail.com>
2022-07-14 10:32:22 -0300

    Knight

--- chess_system/src/application/UI.java
+++ chess_system/src/application/UI.java
@@ -69,27 +69,23 @@ public class UI {
         System.out.println("  a b c d e f g h");
     }
 
-    public static void printMatch(ChessMatch chessMatch, List<ChessPiece> captured){
-        printBoard(chessMatch.getPieces());
-        System.out.println();
-        printCapturedPieces(captured);
-        System.out.println();
-        System.out.println("Turn : " + chessMatch.getTurn());
-
-        if(!chessMatch.getCheck()){
-            
-            System.out.println("Waiting player: " + chessMatch.getCurrentPlayer());
-            
-            if(chessMatch.getCheck()){
-                System.out.println("CHECK! ");
-            }
-            
-        }else{
-            System.out.println("CHECKMATE! ");
-            System.out.println("Winner: " + chessMatch.getCurrentPlayer());
-        }
-        
-    }
+    public static void printMatch(ChessMatch chessMatch, List<ChessPiece> captured) {
+		printBoard(chessMatch.getPieces());
+		System.out.println();
+		printCapturedPieces(captured);
+		System.out.println();
+		System.out.println("Turn : " + chessMatch.getTurn());
+		if (!chessMatch.getCheckMate()) {
+			System.out.println("Waiting player: " + chessMatch.getCurrentPlayer());
+			if (chessMatch.getCheck()) {
+				System.out.println("CHECK!");
+			}
+		}
+		else {
+			System.out.println("CHECKMATE!");
+			System.out.println("Winner: " + chessMatch.getCurrentPlayer());
+		}
+	}
 
     public static void printBoard(ChessPiece[][] pieces, boolean[][] possibleMoves) {
         for (int i = 0; i < pieces.length; i++) {
